3902d8bfacd7572c959c28ebbb8c5cd29b0a47da
Arthur <arthur.oda@fatec.sp.gov.br>
2024-08-28 13:42:26 -0300

    banco & cliente

---
+++ model/Cliente.php
@@ -0,0 +1,63 @@
+<?php
+
+class Cliente {
+    private $nome;
+    private $sobrenome;
+    private $cpf;
+    private $data;
+    private $telefone;
+    private $email;
+    private $senha;
+
+    public function __construct($nome, $sobrenome, $cpf, $data, $telefone, $email, $senha) {
+        $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
+        $this->cpf = $cpf;
+        $this->data = $data;
+        $this->telefone = $telefone;
+        $this->email = $email;
+        $this->senha = $senha;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getSobrenome() {
+        return $this->sobrenome;
+    }
+    public function setSobrenome($sobrenome) {
+        $this->sobrenome = $sobrenome;
+    }
+
+    public function getCpf() {
+        return $this->cpf;
+    }
+    public function setCpf($cpf) {
+        $this->cpf = $cpf;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+    public function setData($data) {
+        $this->data = $data;
+    }
+
+    public function getTelefone() {
+        return $this->telefone;
+    }
+    public function setTelefone($telefone) {
+        $this->telefone = $telefone;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+}
